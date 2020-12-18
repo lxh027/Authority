5437c3e5a249a38b99cf5c6e13b6e62f991aad97
lxh027 <992874705@qq.com>
2020-12-18 15:13:31 +0800

    add: 用户页面注册权限点

--- web/src/js/template.js
+++ web/src/js/template.js
@@ -3,8 +3,9 @@ const red_button = "<button class='layui-btn layui-btn-sm layui-btn-danger data-
 const green_button = "<button class='layui-btn layui-btn-normal layui-btn-sm data-add-btn' lay-event='[Event]'> [Label] </button>"
 const green_link = "<a class='layui-btn layui-btn-normal layui-btn-xs data-count-edit' lay-event='[Event]'>[Label]</a>"
 const red_link = "<a class='layui-btn layui-btn-xs layui-btn-danger data-count-delete' lay-event='[Event]'>[Label]</a>"
+const warm_link = "<a class='layui-btn layui-btn-xs layui-btn-warm data-count-delete' lay-event='[Event]'>[Label]</a>"
 
-let button_array = new Array(red_button, green_button, green_link, red_link)
+let button_array = [red_button, green_button, green_link, red_link, warm_link]
 
 function getButton(item, event, label) {
     const reg = new RegExp("\\[([^\\[\\]]*?)\\]", 'igm');
